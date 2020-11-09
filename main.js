a146ebd6300991ee42f88487429fe0a0cfb2ae41
Fabio Guerrero <fabioandresgv8@gmail.com>
2020-11-09 15:14:00 -0500

    documentacion

---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ *this dunction will accept details about a person
+ *and create an object for them.
+ * @param {String} name  the person's full  name 
+ * @param {Number} age  the person's age
+ * @param {Boolean} isDeveloper  Whether or not the person is a developer
+ * @returns {Object}  The person object
+
+ */
+function createPerson(name, age, isDeveloper){
+    name
+    return {
+        name: name,
+        age: age,
+        isDeveloper
+    } 
+}
+ 
