4f26f79b9812cb14c4b653347fbea9f198575033
Fabio Guerrero <fabioandresgv8@gmail.com>
2020-11-09 16:02:03 -0500

    version 3

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+/** esta es una descripcion de esta funcion foo */
 function foo (){}
-
+/**
+ *
+ *representa un libro
+ * @param {string} titulo - el titulo del libro
+ * @param {string} autor - el autor del libro
+ */
 function libro (titulo, autor){}
-
+/**
+ *soluciona el puzz de torres de hanoi, para cualquier numero de discos.
+ *
+ * @param {string} discos - cuantos discos mover
+ * @param {string} de - el numero pole de inicio
+ * @param {string} a - el nombre de destino
+ * @param {string} extra - el otro nombre del pole
+ */
 const hanoi = (discos, de, a, extra) => {
     if (discos === 1){
         console.log(`Mover disco 1 de post ${a} a post ${a}`);
